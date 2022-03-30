d128f02db87cd7449db429a73d9450950613a0f8
Devvedout <99549144+Devvedout@users.noreply.github.com>
2022-03-30 21:27:31 +0200

    Just moved a comment up a little

--- Calculator.py
+++ Calculator.py
@@ -36,9 +36,9 @@ def __calculator__() :
             os.system('python Calculator.py')
             exit()
 
+    #Adds the numbers up or subtracts them
     e_type = input("Type: ")
 
-    #Adds the numbers up or subtracts them
     add = int(first) + int(second)
     subtract = int(first) - int(second)
     multiply = int(first) * int(second)
